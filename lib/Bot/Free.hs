aa1497935a7fc8451f65c6acfbb9b8a5d931d6b5
André Gustavo Rigon <andregrigon@gmail.com>
2014-05-24 00:06:34 -0300

    Parser using free Applicative

--- lib/Bot/Free.hs
+++ lib/Bot/Free.hs
@@ -2,37 +2,60 @@
 
 module Bot.Free where
 
-import Control.Alternative.Free
-import Control.Applicative
-import Data.List
+import Control.Applicative       ( (<$>), (<*>), pure, many, Applicative )
+import Control.Applicative.Free  ( Ap(..), hoistAp, retractAp )
+import Control.Monad.Trans.State ( StateT(..), runStateT )
 
-data Fun a = Fun 
-  { funName :: String
-  , funValue :: a
+data Arg a = Arg 
+  { argName :: String
+  , readArg :: String -> Maybe a
   } deriving Functor
 
-type FreeA = Alt Fun
+type Reader = Ap Arg
 
-f :: String -> FreeA a
-f name = liftAlt $ Fun name undefined
+type Error = String
 
-a, b, c, d :: FreeA a
-a = f "a"
-b = f "b"
-c = f "c"
-d = f "d"
+type Parser = StateT [String]
 
-sample :: FreeA String
-sample = a <*> b
+showReader :: Reader a -> String
+showReader (Pure _) = ""
+showReader (Ap arg (Pure _)) = argName arg
+showReader (Ap arg rest) = argName arg  ++ " <*> " ++ showReader rest
 
-pretty :: FreeA a -> String
-pretty (Alt []) = ""
-pretty (Alt alts) = paren $ intercalate " <|> " $ map (paren . prettyApp) alts
+runReader :: Reader a -> [String] -> Either Error (a, [String])
+runReader = runParser . liftReader
+
+runParser :: Parser m a -> [String] -> m (a, [String])
+runParser = runStateT
+    
+liftReader :: Reader a -> Parser (Either Error) a
+liftReader = retractAp . hoistAp liftArg
+
+liftArg :: Arg a -> Parser (Either Error) a
+liftArg (Arg name reader) = parseOne (Left $ "Missing argument " ++ name) readArg
   where
-    prettyApp :: AltF Fun a -> String
-    prettyApp (Pure _) = "0"
-    prettyApp (Ap f (Alt [Pure _])) = funName f
-    prettyApp (Ap f a) = funName f ++ " <*> " ++ (pretty a)
+    readArg a = maybe (Left $ "Failed to read " ++ name ++ " from " ++ a) Right (reader a)
 
-    paren :: String -> String
-    paren a = "(" ++ a ++ ")"
+
+
+
+
+
+parseOne :: Applicative m => m a -> (String -> m a) -> Parser m a
+parseOne zero one = StateT $
+  \args -> case args of
+    []     -> (,) <$> zero <*> pure []
+    (a:as) -> (,) <$> one a <*> pure as
+
+parseWord :: (String -> Bool) -> Parser Maybe String
+parseWord cond = parseOne Nothing (\a -> if cond a then Just a else Nothing)
+
+parseTask :: [String] -> Parser Maybe (String, [String])
+parseTask commands = (,) <$> commandName <*> many commandArg
+  where
+    isCommandName = (`elem` commands)
+    commandName = parseWord isCommandName
+    commandArg = parseWord (not . isCommandName)
+
+
+
