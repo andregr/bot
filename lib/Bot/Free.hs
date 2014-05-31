b3b6922df0376befd31c0252ed6d2ca50440c20c
André Gustavo Rigon <andregrigon@gmail.com>
2014-05-31 14:45:05 -0300

    Unified parser

--- lib/Bot/Free.hs
+++ lib/Bot/Free.hs
@@ -2,12 +2,23 @@
 
 module Bot.Free where
 
-import Control.Applicative       ( (<$>), (<*>), pure, many, Applicative )
+import Control.Applicative       ( (<$>), (<*>), pure, Applicative )
 import Control.Applicative.Free  ( Ap(..), hoistAp, retractAp )
 import Control.Arrow             ( (&&&) )
 import Control.Monad.Trans.Class ( lift )
-import Control.Monad.Trans.State ( StateT(..), runStateT, get, put )
-import Data.Maybe                ( isNothing )
+import Control.Monad.Trans.Except ( Except, runExcept, throwE, catchE )
+import Control.Monad.Trans.State ( StateT(..), runStateT, liftCatch )
+
+{-
+  "":                          Help
+  "-commandN arg1 .. argN":    Ok
+  "-commandN arg1 .. arg(<N)": Missing arguments
+  "-commandN arg1 .. arg(>N)": Too many arguments
+  "-commandN .. badArg ..":    Bad argument
+  "-unknown ..":               Unknown command
+  "unknown ..":                Missing command 
+  "goodApp badApp1 badApp2":   Bad application 1
+-}
 
 data Arg a = Arg 
   { argName :: String
@@ -17,82 +28,74 @@ data Arg a = Arg
 type Reader = Ap Arg
 
 type Error = String
+type Errors = [Error]
+
+type Parser = StateT [String] (Except Errors)
 
 showReader :: Reader a -> String
 showReader (Pure _) = ""
 showReader (Ap arg (Pure _)) = argName arg
 showReader (Ap arg rest) = argName arg  ++ " <*> " ++ showReader rest
 
-runReader :: Reader a -> [String] -> Either Error (a, [String])
-runReader = runStateT . liftReader
+runReader :: Reader a -> [String] -> Either Errors (a, [String])
+runReader r as = runExcept $ runStateT (liftReader r) as
+
+liftReader :: Reader a -> Parser a
+liftReader = retractAp . hoistAp liftArg
+
+liftArg :: Arg a -> Parser a
+liftArg (Arg name reader) = consumeOne (missingArgMsg name) readArg
   where
-    liftReader :: Reader a -> StateT [String] (Either Error) a
-    liftReader = retractAp . hoistAp liftArg
+    readArg a = maybe (throwE [readFailedMsg name a]) return (reader a)
 
-    liftArg :: Arg a -> StateT [String] (Either Error) a
-    liftArg (Arg name reader) = parseOne (Left $ missingArgMsg name) readArg
-      where
-        readArg a = maybe (Left $ readFailedMsg name a) Right (reader a)
+    readFailedMsg name a = "Failed to read '" ++ name ++ "' from " ++ a
+    missingArgMsg name = "Missing argument '" ++ name ++ "'"
 
-        readFailedMsg name a = "Failed to read " ++ name ++ " from " ++ a
-        missingArgMsg name = "Missing argument " ++ name
+runParser :: Parser a -> [String] -> Either Errors a
+runParser p as = 
+  case runExcept (runStateT p as) of
+    Left e -> Left e
+    Right (a, []) -> Right a
+    Right (_, unused) -> Left $ ["Unused arguments: '" ++ unwords unused ++ "'"]
 
+runParserPrint :: Parser [IO a] -> String -> IO ()
+runParserPrint p s = either (putStrLn . unlines) sequence_ $ runParser p (words s)
 
-
-
-
-
-parseOne :: Applicative m => m a -> (String -> m a) -> StateT [String] m a
-parseOne zero one = StateT $
-  \args -> case args of
-    []     -> (,) <$> zero <*> pure []
-    (a:as) -> (,) <$> one a <*> pure as
-
-parseWord :: (String -> Bool) -> StateT [String] Maybe String
-parseWord cond = parseOne Nothing (\a -> if cond a then Just a else Nothing)
-
-data Result = Failure | Success
+consumeOne :: Error -> (String -> Except Errors a) -> Parser a
+consumeOne noArgMsg one = StateT consume
+  where
+    consume []     = throwE [noArgMsg]
+    consume (a:as) = (,) <$> one a <*> pure as
 
 data Command = Command
   { commandName :: String
-  , prepareCommand :: StateT [String] (Either Error) Action
+  , prepareCommand :: Parser Action
   }
 
-data Task = Task 
-  { taskCommand :: Command
-  , taskArgs :: [String]
-  }
-
-type Action = IO Result
+type Action = IO ()
 
 instance Show Command where
   show = commandName
 
-instance Show Task where
-  show (Task c as) = show c ++ "(" ++ unwords as ++ ")"
-
-taskParser :: [Command] -> StateT [String] Maybe Task
-taskParser commands = Task <$> parseCommand <*> many parseArg
-  where
-    parseCommand = parseOne Nothing lookupCommand
-    parseArg = parseOne Nothing (\a -> if isNothing (lookupCommand a) then Just a else Nothing)
-    commandMap = map (commandName &&& id) commands
-    lookupCommand = flip lookup commandMap
-
-actionParser :: [Command] -> StateT [String] (Either Error) Action
+actionParser :: [Command] -> Parser Action
 actionParser commands = do
-    as <- get
-    let maybeTask = runStateT (taskParser commands) as
-    (task, unused) <- lift $ maybe (Left $ unknownTasksMsg as) Right maybeTask
-    put unused
-    lift $ actionParser task
+    command <- parseCommand
+    liftCatch catchE (prepareCommand command) $ \e -> 
+      lift $ throwE (wrongArgMsg command : indent e)
   where
-    actionParser :: Task -> Either Error Action
-    actionParser (Task c args) = 
-      case runStateT (prepareCommand c) args of
-        Left e -> Left e
-        Right (a, []) -> Right a
-        Right (_, unused) -> Left $ unusedArgsMsg unused
+    parseCommand = consumeOne noArgsLeftMsg lookupCommand
+    commandMap = map (commandName &&& id) commands
+    lookupCommand a = maybe (throwE [unknownCommandMsg a]) return (lookup a commandMap)
+    indent as = map ("  " ++) as
+    wrongArgMsg command = "Failed to read arguments for command '" ++ commandName command ++ "':"
+    unknownCommandMsg a = "Unknown command '" ++ a ++ "'"
+    noArgsLeftMsg = "No arguments left"
 
-    unknownTasksMsg as = "Failed to parse tasks from " ++ show as
-    unusedArgsMsg as = "Unused arguments: " ++ show as
+greet :: String -> Action
+greet s = putStrLn $ "Hello " ++ s ++ "!"
+
+thank :: String -> Action
+thank s = putStrLn $ "Thank you, " ++ s ++ "."
+
+name :: Parser String
+name = liftArg $ Arg "name" Just
