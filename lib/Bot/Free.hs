84dd27adb46990012c50707a918de5418263d553
André Gustavo Rigon <andregrigon@gmail.com>
2014-05-31 19:17:59 -0300

    Parser newtype and better action parsing

--- lib/Bot/Free.hs
+++ lib/Bot/Free.hs
@@ -1,13 +1,17 @@
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Bot.Free where
 
-import Control.Applicative       ( (<$>), (<*>), pure, Applicative )
+import Control.Applicative       ( Applicative(..), Alternative, (<$>), many )
 import Control.Applicative.Free  ( Ap(..), hoistAp, retractAp )
 import Control.Arrow             ( (&&&) )
+import Control.Monad.Loops ( whileM )
 import Control.Monad.Trans.Class ( lift )
 import Control.Monad.Trans.Except ( Except, runExcept, throwE, catchE )
 import Control.Monad.Trans.State ( StateT(..), runStateT, liftCatch )
+import qualified Control.Monad.Trans.State as S ( get, put )
+import Data.List (isPrefixOf)
 
 {-
   "":                          Help
@@ -28,48 +32,57 @@ data Arg a = Arg
 type Reader = Ap Arg
 
 type Error = String
-type Errors = [Error]
 
-type Parser = StateT [String] (Except Errors)
+newtype Parser a = Parser { unParser :: StateT [String] (Except Error) a }
+  deriving (Functor, Applicative, Alternative, Monad)
+
+catch :: Parser a -> (Error -> Parser a) -> Parser a
+catch p h = Parser $ liftCatch catchE (unParser p) (fmap (unParser) h)
+
+throw :: Error -> Parser a
+throw = Parser . lift . throwE
+
+get :: Parser [String]
+get = Parser S.get
+
+put :: [String] -> Parser ()
+put = Parser . S.put
+
+runParser :: Parser a -> [String] -> Either Error (a, [String])
+runParser p as = runExcept . (flip runStateT as) . unParser $ p
+
+runParserFully :: Parser a -> [String] -> Either Error a
+runParserFully p as = 
+  case runParser p as of
+    Left e -> Left e
+    Right (a, []) -> Right a
+    Right (_, unused) -> Left $ "Unused arguments: '" ++ unwords unused ++ "'"
 
 showReader :: Reader a -> String
 showReader (Pure _) = ""
 showReader (Ap arg (Pure _)) = argName arg
 showReader (Ap arg rest) = argName arg  ++ " <*> " ++ showReader rest
 
-runReader :: Reader a -> [String] -> Either Errors (a, [String])
-runReader r as = runExcept $ runStateT (liftReader r) as
-
 liftReader :: Reader a -> Parser a
 liftReader = retractAp . hoistAp liftArg
+
+arg :: String -> (String -> Maybe a) -> Parser a
+arg name reader = liftArg $ Arg name reader
 
 liftArg :: Arg a -> Parser a
 liftArg (Arg name reader) = consumeOne (missingArgMsg name) readArg
   where
-    readArg a = maybe (throwE [readFailedMsg name a]) return (reader a)
+    readArg w = maybe (Left $ readFailedMsg name w) Right (reader w)
 
-    readFailedMsg name a = "Failed to read '" ++ name ++ "' from " ++ a
+    readFailedMsg name w = "Failed to read '" ++ name ++ "' from " ++ w
     missingArgMsg name = "Missing argument '" ++ name ++ "'"
 
-runParser :: Parser a -> [String] -> Either Errors a
-runParser p as = 
-  case runExcept (runStateT p as) of
-    Left e -> Left e
-    Right (a, []) -> Right a
-    Right (_, unused) -> Left $ ["Unused arguments: '" ++ unwords unused ++ "'"]
-
-runParserPrint :: Parser [IO a] -> String -> IO ()
-runParserPrint p s = either (putStrLn . unlines) sequence_ $ runParser p (words s)
-
-consumeOne :: Error -> (String -> Except Errors a) -> Parser a
-consumeOne noArgMsg one = StateT consume
-  where
-    consume []     = throwE [noArgMsg]
-    consume (a:as) = (,) <$> one a <*> pure as
+debugParser :: Parser [IO a] -> String -> IO ()
+debugParser p s = either putStrLn sequence_ $ runParserFully p (words s)
 
 data Command = Command
   { commandName :: String
-  , prepareCommand :: Parser Action
+  , applyCommand :: Parser Action
   }
 
 type Action = IO ()
@@ -77,19 +90,53 @@ type Action = IO ()
 instance Show Command where
   show = commandName
 
+commandParser :: [Command] -> Parser Command
+commandParser commands = do
+    name <- tail <$> string ? ("-" `isPrefixOf`)
+    case lookup name commandMap of
+      Nothing -> throw $ unknownCommandMsg name
+      Just c  -> return c
+  where
+    commandMap = map (commandName &&& id) commands
+    unknownCommandMsg w = "Unknown command '" ++ w ++ "'"
+
 actionParser :: [Command] -> Parser Action
 actionParser commands = do
-    command <- parseCommand
-    liftCatch catchE (prepareCommand command) $ \e -> 
-      lift $ throwE (wrongArgMsg command : indent e)
+    command <- commandParser commands
+    args <- many $ string ? (not . isPrefixOf "-")
+    let maybeAction = runParser (applyCommand command) args
+    case maybeAction of
+      Right (action, []) -> return action
+      Right (_, unused)  -> throw $ tooManyArgsMsg command unused
+      Left e             -> throw $ wrongArgMsg command ++ "\n\t" ++ e
   where
-    parseCommand = consumeOne noArgsLeftMsg lookupCommand
-    commandMap = map (commandName &&& id) commands
-    lookupCommand a = maybe (throwE [unknownCommandMsg a]) return (lookup a commandMap)
-    indent as = map ("  " ++) as
-    wrongArgMsg command = "Failed to read arguments for command '" ++ commandName command ++ "':"
-    unknownCommandMsg a = "Unknown command '" ++ a ++ "'"
-    noArgsLeftMsg = "No arguments left"
+    wrongArgMsg c = "Failed to read arguments for command '" ++ 
+                    commandName c ++ "':"
+    tooManyArgsMsg c u = "Unused arguments for command '" ++ commandName c ++
+                         "': " ++ unwords u
+
+consumeOne :: Error -> (String -> Either Error a) -> Parser a
+consumeOne noArgMsg one = get >>= consume
+  where
+    consume []         = throw noArgMsg
+    consume (arg:args) = put args >> either throw return (one arg)    
+
+isEndOfInput :: Parser Bool
+isEndOfInput = null <$> get
+
+allRemaining :: Parser a -> Parser [a]
+allRemaining p = whileM (not <$> isEndOfInput) p
+
+string :: Parser String
+string = consumeOne "Missing argument" Right
+
+(?) :: Parser a -> (a -> Bool) -> Parser a
+p ? c = do
+  a <- p
+  if c a 
+    then return a 
+    else throw "Condition failed"
+infixl 6 ?
 
 greet :: String -> Action
 greet s = putStrLn $ "Hello " ++ s ++ "!"
@@ -97,5 +144,13 @@ greet s = putStrLn $ "Hello " ++ s ++ "!"
 thank :: String -> Action
 thank s = putStrLn $ "Thank you, " ++ s ++ "."
 
-name :: Parser String
-name = liftArg $ Arg "name" Just
+repeatPrint :: String -> Int -> Action
+repeatPrint m c = sequence_ $ take c $ repeat $ putStrLn m
+
+commands :: [Command]
+commands = [ Command "greet" (greet <$> arg "name" Just)
+           , Command "thank" (thank <$> arg "name" Just)
+           , Command "repeat" (repeatPrint 
+                                 <$> arg "message" Just 
+                                 <*> arg "count" (Just . read))
+           ]
